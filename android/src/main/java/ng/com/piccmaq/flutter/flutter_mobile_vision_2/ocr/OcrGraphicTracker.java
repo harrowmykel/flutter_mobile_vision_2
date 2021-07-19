271978be55489aaf39325e1cad998a0691772ea5
A. Micheal <mo.aro.vir@gmail.com>
2021-07-19 13:09:12 +0200

    stopped at ui

--- android/src/main/java/ng/com/piccmaq/flutter/flutter_mobile_vision_2/ocr/OcrGraphicTracker.java
+++ android/src/main/java/ng/com/piccmaq/flutter/flutter_mobile_vision_2/ocr/OcrGraphicTracker.java
@@ -4,16 +4,23 @@ import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.text.TextBlock;
 
+
+import android.graphics.RectF;
+
 import ng.com.piccmaq.flutter.flutter_mobile_vision_2.ui.GraphicOverlay;
+import ng.com.piccmaq.flutter.flutter_mobile_vision_2.ui.ScanAreaGraphic;
 
 class OcrGraphicTracker extends Tracker<TextBlock> {
 
     private GraphicOverlay<OcrGraphic> overlay;
     private OcrGraphic graphic;
 
-    public OcrGraphicTracker(GraphicOverlay<OcrGraphic> overlay, OcrGraphic graphic) {
+    private GraphicOverlay<ScanAreaGraphic> scanAreaOverlay;
+
+    public OcrGraphicTracker(GraphicOverlay<OcrGraphic> overlay, OcrGraphic graphic, GraphicOverlay<ScanAreaGraphic> scanAreaOverlay) {
         this.overlay = overlay;
         this.graphic = graphic;
+        this.scanAreaOverlay = scanAreaOverlay;
     }
 
     @Override
@@ -23,8 +30,24 @@ class OcrGraphicTracker extends Tracker<TextBlock> {
 
     @Override
     public void onUpdate(Detector.Detections<TextBlock> detections, TextBlock textBlock) {
-        overlay.add(graphic);
-        graphic.updateItem(textBlock);
+       // Only one ScanAreaGraphic exists in scanAreaOverlay (if scanArea specified by flutter developer)
+       ScanAreaGraphic scanAreaGraphic = this.scanAreaOverlay.getBest(0,0);
+       if (scanAreaGraphic == null) {
+         overlay.add(graphic);
+         graphic.updateItem(textBlock);
+         return;
+       }
+       // Translate textBlock to scanAreaOverlay's scale 
+       RectF scaledTextBlockRectF = scanAreaGraphic.translateRect(new RectF(textBlock.getBoundingBox()));
+       // // Check that detected text fits within scan area in order to add
+       if (scanAreaGraphic.getBoundingBox().contains(scaledTextBlockRectF)) {
+         overlay.add(graphic);
+         graphic.updateItem(textBlock);
+       }
+       // If not, remove potential text graphic from overlay (graphic no longer in scan area)
+       else {
+         overlay.remove(graphic);
+       }
     }
 
     @Override
